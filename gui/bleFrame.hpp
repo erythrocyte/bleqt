a324b22b23b700dae156ed0078c49c99b13c90bc
erythrocyte <marseille.tatar@yahoo.com>
2020-09-11 02:12:35 +0300

    status bar with one label and one progressbar inserted

--- gui/bleFrame.hpp
+++ gui/bleFrame.hpp
@@ -20,16 +20,19 @@
 #include <QLineEdit>
 #include <QSlider>
 #include <QMenu>
+#include <QStatusBar>
+#include <QProgressBar>
 
 using namespace QtCharts;
 
-namespace ble_gui{
-
-class BleFrame : public QMainWindow
+namespace ble_gui
 {
-	Q_OBJECT
+
+	class BleFrame : public QMainWindow
+	{
+		Q_OBJECT
 	public:
-		explicit BleFrame(QWidget* parent = nullptr);
+		explicit BleFrame(QWidget *parent = nullptr);
 		virtual ~BleFrame();
 
 	private slots:
@@ -38,21 +41,24 @@ class BleFrame : public QMainWindow
 		void updateInputData();
 
 	private:
-		QWidget* central;
-		QGridLayout* layout;
-		QChartView* chartView;
-		QChart* chart;
-		QLineSeries* series_press;
-		QLineSeries* series_sat_num;
-		QLineSeries* series_sat_an;
-		QLineSeries* series_sc;
-		QValueAxis* axisX;
-		QValueAxis* axisYPress;
-		QValueAxis* axisYSat;
-		QSlider* slider;
-		QLabel* label;
-		DataWidget* dataWidget;
+		QWidget *central;
+		QGridLayout *layout;
+		QChartView *chartView;
+		QChart *chart;
+		QLineSeries *series_press;
+		QLineSeries *series_sat_num;
+		QLineSeries *series_sat_an;
+		QLineSeries *series_sc;
+		QValueAxis *axisX;
+		QValueAxis *axisYPress;
+		QValueAxis *axisYSat;
+		QSlider *slider;
+		QLabel *label;
+		DataWidget *dataWidget;
 		QMenu *menu;
+		QStatusBar *statusBar;
+		QLabel *statusLabel;
+		QProgressBar *statusProgressBar;
 
 		std::shared_ptr<ble_src::Grid> grd;
 		std::shared_ptr<ble_src::InputData> data;
@@ -62,16 +68,16 @@ class BleFrame : public QMainWindow
 		void set_default_data();
 		void make_grid();
 		void set_initial_cond();
-		std::vector<double> solve_press(const std::vector<double>& s);
-		std::vector<double> solve_satur(const double tau, const std::vector<double>& s);
+		std::vector<double> solve_press(const std::vector<double> &s);
+		std::vector<double> solve_satur(const double tau, const std::vector<double> &s);
 
 		void fill_time_series(bool init, int index);
 
 		void update_time_info(int index);
 
 		void update_sc(bool init);
-};
+	};
 
-}
+} // namespace ble_gui
 
 #endif
