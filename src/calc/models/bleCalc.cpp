f648214f61c8929e8f18cf25341c53a7af3a787b
erythrocyte <marseille.tatar@yahoo.com>
2021-09-29 08:24:10 +0300

    satur calc

--- src/calc/models/bleCalc.cpp
+++ src/calc/models/bleCalc.cpp
@@ -58,53 +58,53 @@ void BleCalc::calc(const std::shared_ptr<mesh::models::Grid> grd,
     std::string mess = common::services::string_format("qan = %.5f, qnum = %.5f, r = %.3f", qan, qnum, perc);
     logging::write_log(mess, logging::kInfo);
 
-    // _wellWorkParams.push_back(std::make_shared<common::models::WellWorkParams>());
+    _wellWorkParams.push_back(std::make_shared<common::models::WellWorkParams>());
 
-    // while (sumT < data->model->period) {
-    //     if (pressIndex == 0 || pressIndex == data->satSetts->pN) {
-    //         p = services::solve_press(grd, s_prev, data);
-    //         services::calc_u(p, s_prev, data, grd);
-    //         //save_press(index, grd, p);
-    //         pressIndex = 0;
-    //     }
-    //     pressIndex++;
+    while (sumT < data->model->period) {
+        if (pressIndex == 0 || pressIndex == data->satSetts->pN) {
+            p = services::solve_press(grd, s_prev, data);
+            services::calc_u(p, s_prev, data, grd);
+            //save_press(index, grd, p);
+            pressIndex = 0;
+        }
+        pressIndex++;
 
-    //     double t = services::get_time_step(grd, s_prev, data);
-    //     std::string mess = common::services::string_format("tau = %.8f", t);
-    //     logging::write_log(mess, logging::kInfo);
-    //     if (saveT + t >= data->model->save_field_step) {
-    //         t = data->model->save_field_step - saveT;
-    //         saveT = 0.0;
-    //         need_save_fiels = true;
-    //     } else {
-    //         saveT += t;
-    //         need_save_fiels = false;
-    //     }
+        double t = services::get_time_step(grd, s_prev, data);
+        std::string mess = common::services::string_format("tau = %.8f", t);
+        logging::write_log(mess, logging::kInfo);
+        if (saveT + t >= data->model->save_field_step) {
+            t = data->model->save_field_step - saveT;
+            saveT = 0.0;
+            need_save_fiels = true;
+        } else {
+            saveT += t;
+            need_save_fiels = false;
+        }
 
-    //     sumU += services::getULiqInject(grd, data->grd->type) * t;
-    //     s_cur = services::solve_satur(t, s_prev, data, grd);
-    //     s_prev = s_cur;
-    //     sumT += t;
+        sumU += services::getULiqInject(grd, data->grd->type) * t;
+        s_cur = services::solve_satur(t, s_prev, data, grd);
+        s_prev = s_cur;
+        sumT += t;
 
-    //     if (need_save_fiels) {
-    //         std::vector<std::tuple<double, double>> xs_an = services::get_satur_exact(sc, sumU, data);
-    //         auto d = std::make_shared<ble::src::common::models::DynamicData>();
-    //         d->t = sumT;
-    //         d->p = p;
-    //         d->s = s_cur;
-    //         d->s_an = xs_an;
-    //         d->p_ex = _results->data[0]->p_ex;
+        if (need_save_fiels) {
+            std::vector<std::tuple<double, double>> xs_an = services::get_satur_exact(sc, sumU, data);
+            auto d = std::make_shared<ble::src::common::models::DynamicData>();
+            d->t = sumT;
+            d->p = p;
+            d->s = s_cur;
+            d->s_an = xs_an;
+            d->p_ex = _results->data[0]->p_ex;
 
-    //         _results->data.push_back(d);
-    //         index++;
-    //     }
+            _results->data.push_back(d);
+            index++;
+        }
 
-    //     auto wwp = services::calc_well_work_param(grd, s_cur, data->phys, sumT);
-    //     _wellWorkParams.push_back(wwp);
+        auto wwp = services::calc_well_work_param(grd, s_cur, data->phys, sumT);
+        _wellWorkParams.push_back(wwp);
 
-    //     double perc = std::min(100.0, (sumT / data->model->period * 100.0));
-    //     set_progress(perc);
-    // }
+        double perc = std::min(100.0, (sumT / data->model->period * 100.0));
+        set_progress(perc);
+    }
 
     set_progress(100); // completed;
 }
