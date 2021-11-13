ac853aeab51cb5953c7b9895838bdf36cce77b4c
erythrocyte <marseille.tatar@yahoo.com>
2021-11-13 23:19:06 +0300

    fixed

--- src/calc/services/saturSolverAnalytic.cpp
+++ src/calc/services/saturSolverAnalytic.cpp
@@ -13,24 +13,25 @@ namespace ble::src::calc::services {
 std::vector<std::tuple<double, double>> get_satur_exact(const double sc, const double u,
     const std::shared_ptr<common::models::InputData> data)
 {
-    auto get_xs = [&](const double s)
-    {
+    auto get_xs = [&](const double s) {
         double ksi0 = data->grd->rc, poro = data->phys->poro;
         double fu = u / poro * cs::rp::get_dfbl(s, data->phys);
         switch (data->grd->type) {
         case common::models::GridType::kRegular:
             return ksi0 - fu;
-        case common::models::GridType::kRadial:
-            return std::sqrt(ksi0 * ksi0 - 2.0 * fu);
+        case common::models::GridType::kRadial: {
+            double a = ksi0 * ksi0 - 2.0 * fu;
+            return a < 0.0
+                ? 0.0
+                : std::sqrt(ksi0 * ksi0 - 2.0 * fu);
+        }
         default:
             return 0.0;
         }
     };
 
     double xsc = get_xs(sc);
-
     std::vector<std::tuple<double, double>> result;
-
     result.push_back(std::make_tuple(0.0, 0.0));
     result.push_back(std::make_tuple(xsc, 0.0));
 
